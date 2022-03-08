73d30ad362b04444e01a6305a2519d36be0d775e
Limuran <strelgl2@gmail.com>
2022-03-08 20:41:37 +1000

    Бесполезный коммит

--- 4/print.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATRIX_PRINT_H
-#define MATRIX_PRINT_H
-
-#include "matrix.h"
-
-void printMatrix(const Matrix &matrix);
-
-#endif //MATRIX_PRINT_H
--- 4/print_console.cpp
+++
@@ -1,7 +0,0 @@
-#include "print_ostream.h"
-#include "matrix.h"
-#include <iostream>
-
-void printMatrix(const Matrix &matrix) {
-    printMatrix(std::cout, matrix);
-}
